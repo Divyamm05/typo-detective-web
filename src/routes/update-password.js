aa8c9452168d7c498df1f448510882b462d44002
Divyam <divyam2003ds@gmail.com>
2025-04-11 14:11:24 +0530

    commit

--- src/routes/update-password.js
+++ src/routes/update-password.js
@@ -13,6 +13,7 @@ const db = await mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 router.post("/update-password", async (req, res) => {
